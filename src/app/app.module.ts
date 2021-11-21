4129395d6347dcbf2f62805f23b500ee631142d5
ankurinfoniti <ankur.infoniti@gmail.com>
2021-11-21 14:36:24 +0530

    interceptors setup for http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { GaugeModule } from "angular-gauge";
 import { MatTabsModule } from "@angular/material/tabs";
@@ -12,8 +12,11 @@ import { MatSelectModule } from "@angular/material/select";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { HomeComponent } from './components/home/home.component';
+import { SearchBarComponent } from "./components/search-bar/search-bar.component";
+import { HomeComponent } from "./components/home/home.component";
+
+import { HttpErrorsInterceptor } from "./interceptors/http-errors.interceptor";
+import { HttpHeadersInterceptor } from "./interceptors/http-headers.interceptor";
 
 @NgModule({
   declarations: [AppComponent, SearchBarComponent, HomeComponent],
@@ -29,7 +32,18 @@ import { HomeComponent } from './components/home/home.component';
     MatFormFieldModule,
     MatSelectModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
