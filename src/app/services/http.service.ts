98f1c40b58834f268feb3a73070d684c2aad3f07
ankurinfoniti <ankur.infoniti@gmail.com>
2021-11-23 00:14:56 +0530

    details page with style

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { forkJoin, Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { environment as env } from "src/environments/environment";
 import { APIResponse, Game } from "../models";
 
@@ -23,5 +24,40 @@ export class HttpService {
     return this.http.get<APIResponse<Game>>(`${env.BASE_URL}/games`, {
       params: params,
     });
+  }
+
+  getGameDetails(id: string): Observable<Game> {
+    const gameInfoRequest = this.http.get(`${env.BASE_URL}/games/${id}`);
+    const gameTrailersRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/movies`
+    );
+    const gameScreenshotsRequest = this.http.get(
+      `${env.BASE_URL}/games/${id}/screenshots`
+    );
+
+    return forkJoin([
+      gameInfoRequest,
+      gameScreenshotsRequest,
+      gameTrailersRequest,
+    ]).pipe(
+      map((resp: any) => {
+        return {
+          id: resp[0]["id"],
+          background_image: resp[0]["background_image"],
+          name: resp[0]["name"],
+          released: resp[0]["released"],
+          metacritic_url: resp[0]["metacritic_url"],
+          website: resp[0]["website"],
+          description: resp[0]["description"],
+          metacritic: resp[0]["metacritic"],
+          genres: resp[0]["genres"],
+          parent_platforms: resp[0]["parent_platforms"],
+          publishers: resp[0]["publishers"],
+          ratings: resp[0]["ratings"],
+          screenshots: resp[1]["results"],
+          trailers: resp[2]["results"],
+        };
+      })
+    );
   }
 }
